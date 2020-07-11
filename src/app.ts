cddf30009f61b3bdafda23ccc0950b7c72464f01
Eric Lowry <ericmichaellowry@gmail.com>
2020-07-11 10:16:04 -0700

    Completed project item class

--- src/app.ts
+++ src/app.ts
@@ -122,8 +122,30 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 // ProjectItem Class
 
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
-  constructor() {
-    super();
+  private project: Project;
+
+  get persons() {
+    if (this.project.people === 1) {
+      return '1 Person';
+    } else {
+      return `${this.project.people} People`;
+    }
+  }
+
+  constructor(hostId: string, project: Project) {
+    super('single-project', hostId, false, project.id);
+    this.project = project;
+
+    this.configure();
+    this.renderContent();
+  }
+
+  configure() {}
+
+  renderContent() {
+    this.element.querySelector('h2')!.textContent = this.project.title;
+    this.element.querySelector('h3')!.textContent = this.persons + ' Assigned';
+    this.element.querySelector('p')!.textContent = this.project.description;
   }
 
 }
@@ -164,9 +186,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement>{
     const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
     listEl.innerHTML = '';
     for (const projItem of this.assignedProjects) {
-      const listItem = document.createElement('li');
-      listItem.textContent = projItem.title;
-      listEl?.appendChild(listItem);
+      new ProjectItem(this.element.querySelector('ul')!.id, projItem);
     }
   }
 }
